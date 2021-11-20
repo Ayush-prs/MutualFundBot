0f738a9c1270a520e4292463ea98e173f2eb4a17
Ayush <qwert_316@yahoo.co.in>
2021-11-20 11:04:46 +0530

    1) reading strings from env variables 2) changed to sync as  http request not executing

--- mutualfundbotjs/jsbot/index.js
+++ mutualfundbotjs/jsbot/index.js
@@ -2,14 +2,18 @@ const request = require('request');
 const http = require('https');
 const util = require('util');
 
-module.exports = async function (context, myTimer, inputDocument) {
+module.exports = function (context, myTimer, inputDocument) {
   var timeStamp = new Date().toISOString();
-  const SUBSCRIPTION_KEY = '054e16cac30b480f8fa0d28f7e327ef6';
-  const telegram_chatid = '1596195885';
-  const telegram_bot_token = '1634029805:AAH-s3C9vQqOqqPOO-r3ivV9X_OKHY_dR4U';
+  const telegram_chatid = process.env["Telegram_Chat_ID"];
+  const telegram_bot_token = process.env["Telegram_Bot_Token"];  ;
+  const bing_endpoint = process.env["Bing_Endpoint"];
+  const subscription_key = process.env["Bing_Subscription_Key"];
+
   const telegram_url_template = 'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message};'
-
-
+  context.log(telegram_chatid);
+  context.log(telegram_bot_token);
+  context.log(bing_endpoint);
+  context.log(subscription_key);
   if (myTimer.isPastDue) {
     context.log('JavaScript is running late!');
   }
@@ -17,18 +21,25 @@ module.exports = async function (context, myTimer, inputDocument) {
   inputDocument.forEach(data => {
     let query = data.searchquery.replace(" ", "+");
     const options = {
-      url: 'https://api.bing.microsoft.com//v7.0/search?q=' + query,
+      url: bing_endpoint + query,
       headers: {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY
+        'Ocp-Apim-Subscription-Key': subscription_key
       }
     }
 
     request(options, function (err, response, msg) {
-      context.log('extraction......');
+      context.log('inside request method......');
       const value = JSON.parse(response.body)["webPages"]["value"][0].name.split('[')[1].split(']')[0];
       let message = data.fundname + ' : ' + value;
-      let telegramUrl = telegram_url_template.replace('{token}',telegram_bot_token ).replace('{chat_id}',telegram_chatid).replace('{message',message);
+      let telegramUrl = telegram_url_template.replace('{token}',telegram_bot_token ).replace('{chat_id}',telegram_chatid).replace('{message}',message);
       request.post(telegramUrl)
+      .on('response', function(response) {
+        context.done();
+      })
+      .on('error', function(err) {
+        context.log('telegram message failed : ' + err)
+        context.done();
+      })
     })
   });
 };
